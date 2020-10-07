e361f075713a287e8b0c5303073eb1e80c39e6b7
ffffff-Fox <72293124+ffffff-Fox@users.noreply.github.com>
2020-10-06 23:33:09 -0600

    Create basic_functions.sol

---
+++ basic_functions.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.5.13;
+
+contract WorkingWithVariables {
+    uint256 public myUint;
+    
+    function setMyUint(uint _myUint) public {
+        myUint = _myUint;
+    }
+    
+    bool public myBool;
+    
+    function setMyBool(bool _myBool) public {
+        myBool = _myBool;
+    }
+}
